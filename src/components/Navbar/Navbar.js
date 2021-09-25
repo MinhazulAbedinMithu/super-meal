2a15263bcccabb7f6f5588bff230d32ebf95bd26
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2021-09-25 19:18:20 +0600

    fix navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,10 +18,10 @@ const Navbar = () => {
 					<div id="bar-3" className="bar"></div>
 				</div>
 				<ul className="nav-ul">
-					<Link to="/">Home</Link>
-					<Link to="/about">About</Link>
-					<Link to="/shop">Shop</Link>
-					<Link to="/">Orders</Link>
+					<li><Link to="/" className="nav-link">Home</Link></li>
+					<li><Link to="/about" className="nav-link">About</Link></li>
+					<li><Link to="/shop" className="nav-link">Shop</Link></li>
+					<li><Link to="/" className="nav-link">Orders</Link></li>
 				</ul>
 			</div>
 		</nav>
